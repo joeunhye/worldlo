4abf5ce5fb7db233a5c2aab99568f65ba67f6f8f
eunhye919 <eunhye919@vtex.co.kr>
2022-02-22 16:36:30 +0900

    test

--- js/common.js
+++ js/common.js
@@ -2,6 +2,14 @@ let winW;
 let scrollH;
 const headEl = document.querySelector('#header');
 
+// 모바일 웹 주소창 숨기기
+window.addEventListener('load', function() {
+    // body의 height를 살짝 늘리는 코드
+    document.body.style.height = (document.documentElement.clientHeight + 5) + 'px';
+    // scroll를 제어 하는 코드
+    setTimeout(scrollTo, 0, 0, 1);
+  }, false);
+
 window.addEventListener('scroll', () => {
     changeHeadColor();
     size();
