4cee1d38fc7828de96e0750a458b1c11d98e717a
eunhye919 <eunhye919@vtex.co.kr>
2022-02-23 09:51:22 +0900

    mobile vh test

--- js/common.js
+++ js/common.js
@@ -160,8 +160,10 @@ function mbMenuChk() {
     }
 }
 
+//MOBILE 100VH SETTING
 function setScreenSize() {
 	let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    console.log(vh)
 }
 setScreenSize();
